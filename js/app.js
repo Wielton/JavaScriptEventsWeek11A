3a0fef8f0aa25f8c4ee40c3827a332e491ce7aa0
Wielton <grantwielgosz@gmail.com>
2022-03-30 02:55:12 -0600

    Managed to retrieve test player data for college Bball and display the first names for each

---
+++ js/app.js
@@ -0,0 +1,71 @@
+/*
+fetch('https://api.fantasynerds.com/v1/nfl/adp?apikey=TEST', {
+    mode: 'no-cors'
+})
+.then((data) => {
+    return data.json();
+    
+})
+function successFunction(response) {
+    console.log(response);
+    console.log(response.data);
+    // Change the page however you want here if needed
+    }
+function failureFunction(error) {
+    console.log(error);
+    // Change the page however you want here if needed
+    }
+axios.request({
+    method: 'GET',
+    
+    url: 'https://www.balldontlie.io/api/v1/players'
+}).then(successFunction).catch(failureFunction);
+*/
+
+
+
+// I successfully retrieved the data from balldontlie.io
+function successFunction(response) {
+    console.log(response);
+    console.log(response.data.data);
+    // Change the page however you want here if needed
+    const players = response.data.data;
+    const playersDiv = document.getElementById('playerName');
+    players.forEach(player => {
+        const playerElement = document.createElement('p');
+        playerElement.innerText = `Player Name: ${player.first_name}`;
+        playersDiv.append(playerElement);
+    })
+    }
+function failureFunction(error) {
+    console.log(error);
+    // Change the page however you want here if needed
+    }
+let players = axios.request({
+        method: 'GET',
+        
+        url: 'https://www.balldontlie.io/api/v1/players'
+    }).then(successFunction).catch(failureFunction);
+/*    
+    axios.request({ 
+    url: 'https://api.fantasynerds.com/v1/nfl/adp?apikey=TEST',
+    method: 'POST',
+    mode: 'no-cors'
+    
+
+    }).then(successFunction).catch(failureFunction);
+console.log(players);
+
+fetch('https://www.balldontlie.io/api/v1/players')
+    .then(response => response.json())
+    .then(players => showPlayers(players.results));
+    
+showPlayers = players => {
+    const playersDiv = document.getElementById('playerName');
+    players.forEach(player => {
+    const playerElement = document.createElement('p');
+    playerElement.innerText = `Player Name: ${player.first_name}`;
+    playersDiv.append(playerElement);
+        });
+}
+*/ 
